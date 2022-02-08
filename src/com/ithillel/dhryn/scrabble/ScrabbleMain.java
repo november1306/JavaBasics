3fe93d9b957023ca05b17c683a917ca7f5fa6999
dmytro.hrynevych <dmytro.hrynevych@hermes-europe.co.uk>
2022-02-09 01:02:47 +0200

    Scrabble game add try/catch for reading file; refactoring

--- src/com/ithillel/dhryn/scrabble/ScrabbleMain.java
+++ src/com/ithillel/dhryn/scrabble/ScrabbleMain.java
@@ -3,8 +3,7 @@ package com.ithillel.dhryn.scrabble;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Scanner;
+import java.util.*;
 
 /**
  * input: taaac
@@ -14,7 +13,7 @@ import java.util.Scanner;
  */
 
 public class ScrabbleMain {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
         //read phrase from console
         String phrase = "taaac";
@@ -27,18 +26,39 @@ public class ScrabbleMain {
         //  t -> 1
         //  a -> 3
         //  c -> 1
-        HashMap<Character, Integer> phraseCharCount = getCharacterCountMap(phrase);
 
-        //read from dictionary line by line
-        FileReader fileReader = new FileReader("attachments/dictionary.txt");
-        BufferedReader reader = new BufferedReader(fileReader);
+        //read from dictionary to ArrayList
+        List<String> wordsList = new LinkedList<>();
+        String fileName = "attachments\\dictionary.txt";
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(fileName));
+            try {
+                wordsList = readWords(reader);
+            } finally {
+                reader.close();
+            }
+        } catch (IOException ex) {
+            System.err.println(" problem reading file from " + System.getProperty("user.dir") + "\\" + fileName);
+            ex.printStackTrace();
+        }
 
+        searchWordsExistInPhrase(phrase, wordsList);
+    }
+
+    private static List<String> readWords(BufferedReader reader) throws IOException {
+        List<String> wordsList = new LinkedList<>();
         String word;
-        boolean canMakeWord;
         while ((word = reader.readLine()) != null) {
-            // check if we can make word
-            canMakeWord = true;
+            wordsList.add(word);
+        }
+        return wordsList;
+    }
+
+    public static void searchWordsExistInPhrase(String phrase, List<String> wordsList) {
+        HashMap<Character, Integer> phraseCharCount = getCharacterCountMap(phrase);
+        for (String word : wordsList) {
             HashMap<Character, Integer> wordCharCount = getCharacterCountMap(word);
+            boolean canMakeWord = true;
             for (char ch : wordCharCount.keySet()) {
                 int wordChCount = wordCharCount.get(ch);
                 int phraseChCount = phraseCharCount.containsKey(ch) ?
@@ -49,7 +69,6 @@ public class ScrabbleMain {
             if (canMakeWord && word.length() > 2)
                 System.out.println("YES we can " + word);
         }
-
     }
 
     private static HashMap<Character, Integer> getCharacterCountMap(String phrase) {
